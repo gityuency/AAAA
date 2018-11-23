1645361f206577149f5bcd54fc5f62fc8f2aef7f
CRF <yuanxin_yang@crfchina.com>
2018-11-23 16:32:50 +0800

    第一次提交

---
+++ GGG/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GGG
+//
+//  Created by ChinaRapidFinance on 2018/11/23.
+//  Copyright © 2018年 yuency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
